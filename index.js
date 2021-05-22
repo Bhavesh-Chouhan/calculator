ec782a8d5b15790e1df6e5c6f55a64b744ccdc6c
Bhavesh Chouhan <bhaveshchouhan645@gmail.com>
2021-05-22 11:00:38 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const clr = () => {
+  document.getElementById("result").value = "";
+};
+const display = (val) => {
+  document.getElementById("result").value += val;
+};
+const equate = () => {
+  let x = document.getElementById("result").value;
+  let y = eval(x);
+  document.getElementById("result").value = y;
+};
